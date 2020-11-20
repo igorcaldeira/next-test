3abc8ad780b886ab9f802d9b431b76a0e30873f0
Igor Caldeira <igor.brandao@softplan.com.br>
2020-11-20 13:33:37 -0300

    new sample changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 // const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  //   basePath: "/",
-  //   assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
-  //   compress: false,
-};
+// module.exports = {
+//   basePath: "/",
+//   assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
+//   compress: false,
+// };
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+});
